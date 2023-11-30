110750b59fbff212be96cd7fcb0e313168cc9ade
Akmalrbn <akmal.rabbani@ui.ac.id>
2023-11-30 09:50:23 +0700

    Update Edit.php dan register

--- register.php
+++ register.php
@@ -1,32 +1,78 @@
-<?php
-$host = 'localhost'; // or your host name
-$dbUsername = 'root'; // your database username
-$dbPassword = ''; // your database password
-$dbName = 'your_database_name'; // your database name
+!DOCTYPE html>
+<html lang="en">
 
-// Create database connection
-$conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Register</title>
+</head>
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+<body>
+    <div class="container">
+        <div class="box form-box">
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $conn->real_escape_string($_POST['username']);
-    $password = password_hash($conn->real_escape_string($_POST['password']), PASSWORD_DEFAULT);
-    $email = $conn->real_escape_string($_POST['email']);
+            <?php
 
-    // Insert query
-    $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
+            include("php/config.php");
+            if (isset($_POST['submit'])) {
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
 
-    // Execute query
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
+                //verifying the unique email
+            
+                $verify_query = mysqli_query($con, "SELECT Email FROM users WHERE Email='$email'");
 
-$conn->close();
-?>
+                if (mysqli_num_rows($verify_query) != 0) {
+                    echo "<div class='message'>
+                      <p>This email is used, Try another One Please!</p>
+                  </div> <br>";
+                    echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+                } else {
+
+                    mysqli_query($con, "INSERT INTO users(Username,Email,Password) VALUES('$username','$email','$password')") or die("Erroe Occured");
+
+                    echo "<div class='message'>
+                      <p>Registration successfully!</p>
+                  </div> <br>";
+                    echo "<a href='index.php'><button class='btn'>Login Now</button>";
+
+
+                }
+
+            } else {
+
+                ?>
+
+                <header>Sign Up</header>
+                <form action="" method="post">
+                    <div class="field input">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" autocomplete="off" required>
+                    </div>
+
+                    <div class="field input">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" autocomplete="off" required>
+                    </div>
+                    <div class="field input">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" autocomplete="off" required>
+                    </div>
+
+                    <div class="field">
+
+                        <input type="submit" class="btn" name="submit" value="Register" required>
+                    </div>
+                    <div class="links">
+                        Already a member? <a href="index.php">Sign In</a>
+                    </div>
+                </form>
+            </div>
+        <?php } ?>
+    </div>
+</body>
+
+</html>
