14d10027b536cc940657d962427ac61796535681
ceciliainez11 <cecilia.inez@ui.ac.id>
2023-11-30 22:58:01 +0700

    client.php

---
+++ clients.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>My Clients</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <div class="box form-box">
+            <h2>List of Clients</h2>
+            <a class="btn btn-primary" href="search.php" role="button">Search Client</a>
+            <a class="btn btn-primary" href="create.php" role="button">New Client</a>
+            <br>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Created At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $database = "pentest";
+
+                    //Create Connection
+                    $connection = new mysqli($servername, $username, $password, $database);
+
+                    //Check Connection
+                    if ($connection->connect_error) {
+                        die("Connection failed: " . $connection->connect_error);
+                    }
+
+                    //read all row from database table
+                    $sql = "SELECT * FROM `clients`";
+                    $result = $connection->query($sql);
+
+                    if (!$result) {
+                        die("Invalid query: " . $connection->error);
+                    }
+
+                    //read data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "
+                        <tr>
+                        <td>$row[id]</td>
+                        <td>$row[name]</td>
+                        <td>$row[email]</td>
+                        <td>$row[phone]</td>
+                        <td>$row[address]</td>
+                        <td>$row[created_at]</td>
+                    </tr>
+                        ";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
