973cc0a65780084b25a3d2d1dac1ff7b2cfc4dda
Akmalrbn <akmal.rabbani@ui.ac.id>
2023-11-30 17:01:45 +0700

    Add Search Engine

---
+++ search.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Search Engine</title>
+
+<style type="text/css">
+
+    body{
+        background-color: #F0F8FF;
+        margin-top: -80px;
+    }
+    form {
+        margin: 25%; 
+    }
+</style> 
+    </head> 
+
+<body>
+    <form action="result.php" method="post">
+        <input type="text" name="user_query" size="80" placeholder="Search Image"/>
+        <input type="submit" name="search" value="Search">
